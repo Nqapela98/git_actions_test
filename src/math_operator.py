6fc745df6713d06bbbcfbdcdf606e78ed7c17479
tshepo <nqapelatshepo.com@gmail.com>
2025-05-10 16:21:49 +0200

    added_functions

--- src/math_operator.py
+++ src/math_operator.py
@@ -1,2 +1,13 @@
 import pandas
 import pytest
+
+def add(a, b):
+    """
+    Adds two numbers together.
+    """
+    return a + b
+def sub(a, b):
+    """
+    Subtracts the second number from the first.
+    """
+    return a - b
